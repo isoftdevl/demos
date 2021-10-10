9a5ac5ffbce3907a3f3255572aa90de14e9f4a0c
dGheek <chijiokedgeek@gmail.com>
2021-10-10 07:48:28 -0700

    dipro
    new courier script

---
+++ dipro/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "bitmzgue_dipro");
+define("DB_USER", "bitmzgue_superAdmin");
+define("DB_PASS", "Passme4real");
+define('APP_URL', '');
+?>
